23451d27790c339496c9807e4330d325501b6dd2
tcruz97 <turnercruz@u.boisestate.edu>
2018-09-14 15:22:46 -0600

    Added request to include creds
    this is still not working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -120,7 +120,7 @@ export class HomePage {
 
 
   register(){
-    var link = 'https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=228NH4&redirect_uri=http%3A%2F%2Flocalhost:8100%2Fkidfit%2Fhandle_redirect.php&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&prompt=login consent&state=';
+    var link = 'https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=228NH4&redirect_uri=http%3A%2F%2Flocalhost:8100%2Fkidfit%2Fhandle_redirect.php&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&prompt=login&withcredentials=true consent&state=';
     
     this.getRegisterUserDetails().then(
     data => {
